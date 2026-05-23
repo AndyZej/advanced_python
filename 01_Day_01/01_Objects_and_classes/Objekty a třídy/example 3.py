f11e4bcb12b8a4219ac2c7efcba8156c7f8424aa
AndyZej <kdlab@seznam.cz>
2026-05-23 13:39:55 +0200

    Merge remote-tracking branch 'github/main'

--- 01_Day_01/01_Objects_and_classes/Objekty a třídy/example 3.py
+++ 01_Day_01/01_Objects_and_classes/Objekty a třídy/example 3.py
@@ -1,8 +1,33 @@
 
 
 class BankAccount:
-    def __init__(self, name, balance):
-        self.number = number
-        self.cash = cash
+    def __init__(self, number: int):
+        self.__number = number
+        self.__cash: float = 0.0
 
-    deposit_cash(amount)
+    def get_number(self) -> int:
+        return self.__number
+
+    def deposit_cash(self, amount: float):
+        if amount < 0:
+            raise ValueError("Negative amounts")
+
+        self.__cash += amount
+
+
+    def withdraw_cash(self, amount: float) -> float:
+        if amount > self.__cash:
+            output = self.__cash
+            self.__cash = 0
+            return output
+
+        self.__cash -= amount
+        return amount
+
+    def print_info(self):
+        print(f"account: {self.__number} Cash: {self.__cash}")
+
+
+a = BankAccount(10)
+a.deposit_cash(100)
+print(a.get_number())(amount)
--- 01_Day_01/01_Objects_and_classes/Objekty a třídy/example 4.py
+++ 01_Day_01/01_Objects_and_classes/Objekty a třídy/example 4.py
@@ -1,11 +1,29 @@
 
 
 class Employee:
-
-    def __init__(self, name, salary):
-        self.id = id(self)
+    def __init__(self, id: int, first_name: str, last_name: str):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
-        self.salary = salary
+        self.__salary = 0
 
-set_salary(salary)salary
+    @property
+    def salary(self):
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary: int | float) -> None:
+        if not (isinstance(salary, int) or isinstance(salary, float)):
+            raise TypeError("Salary must be a float")
+
+        if salary < 0:
+            raise ValueError("Salary must be positive")
+
+        self.__salary = salary
+
+
+
+employee1 = Employee(1, "Adam", "Npvak")
+print(employee1.salary)
+employee1.salary = 100
+print(employee1.salary)
